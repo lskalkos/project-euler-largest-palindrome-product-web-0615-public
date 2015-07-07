32bad5de3214aaa19313f06810b1d3b502f453e4
Lia Skalkos <hello@liaskalkos.net>
2015-07-07 09:59:34 -0400

    Slight refactor

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -4,14 +4,14 @@ class LargestPalindromeProduct
 
   def answer
     multiples_set1 = (100..999).to_a
-    multiples_set2 = (100..999).to_a
+    multiples_set2 = multiples_set1
 
     palindromes = []
 
 
     multiples_set1.each do |set1_num|
 
-      multiples_set2.each do |set2_num|
+      multiples_set2.select do |set2_num|
         product = set1_num * set2_num
 
         num_string = product.to_s
@@ -21,10 +21,8 @@ class LargestPalindromeProduct
           first_half = num_string.slice(0, (num_length/2))
           second_half = num_string.slice((num_length/2), num_length)
 
-          if first_half.reverse == second_half
-            palindrome = product
-            palindromes << palindrome
-          end
+
+          palindromes << product if first_half.reverse == second_half
         end
 
       end
